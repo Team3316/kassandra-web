4f877b40f77cb7217168dacb41e5317424816928
Ofir <frc3316@gmail.com>
2017-02-19 20:57:03 +0200

    finished atounomous pageeeee.

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -4,18 +4,44 @@ app.controller('ctr' ,function ($scope, $http, $cookies) {
     $scope.matches = [];
     $scope.teams = [];
     
+    //canvas for field
+    var canvas;
+    var context;
+    
+    //intialize canvas shit
+    angular.element(document).ready(function () {
+        canvas = document.getElementById('myCanvas');
+        context = canvas.getContext('2d');
+    }); 
+
+    //headers for api calls
     var config = {
         headers: {
             'Content-Type' : 'application/json; charset="utf-8"',
             'X-TBA-App-Id' : '3316:Kassandra:2.0'
         }
     };
+
+    var coordinates = [];
         
     $scope.accessToken = "OMRI_GRANTED";
 
-    $scope.checkboxCrossLine = {
-       crossLine : false
-     };
+    $scope.addOnClick = function(event){
+        var x = event.offsetX;
+        var y = event.offsetY;
+        //console.log("x: " + x + ", y: " + y);
+        coordinates.push({x, y});
+        console.log(coordinates);       
+        context.beginPath();
+        context.strokeStyle = "#e74c3c";
+        context.arc(x, y, 10, 0, 2 * Math.PI, false);
+        context.stroke();
+    }
+
+    $scope.clear = function(){
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        coordinates = [];
+    }
 
     $scope.checkPass = function(){
         var pass = document.getElementById("pass").value;
@@ -94,7 +120,7 @@ app.controller('ctr' ,function ($scope, $http, $cookies) {
             location.href = "/#/autonomous/" + m + "/" + t;
     }
 })
-    .config(function ($mdThemingProvider, $stateProvider) {
+.config(function ($mdThemingProvider, $stateProvider) {
         $mdThemingProvider.theme('default')
             .primaryPalette('cyan')
             .accentPalette('teal');
