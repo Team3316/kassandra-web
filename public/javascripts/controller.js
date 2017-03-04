8d687f8d5d52ee7004841a810aff25877b31a8ff
DBug <frc3316@gmail.com>
2017-03-04 13:21:30 +0200

    lol'

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -287,6 +287,7 @@ app.controller('ctr', function ($scope, $http, $cookies, $location) {
         var match = btn.value;
         console.log("avad!!!");
         $location.path('/report/'+team+'/'+match);
+        $scope.apply();
     }
 
     $scope.finalButton = function (generalComments) {
