42575b9c55272ab0df081919bcce5dc816402e8b
Roey Ripstein <Roey Ripstein>
2017-03-25 20:38:54 +0300

    Planters are sorted by gears (not by percent)

--- db/mongo.js
+++ db/mongo.js
@@ -182,7 +182,7 @@ exports.getTopPlanters = function (res){
 		},
     { $match: { attempts: { $gt: 0 } } },
 		{ $project: { _id: 1, plant_pct: { $divide: ["$successes", "$attempts"] }, successes: 1, attempts: 1 } },
-		{ $sort: { plant_pct: -1 } },
+		{ $sort: { successes: -1 } },
 		{ $limit: 24 }
 	], function (err, doc) {
     res.send(JSON.stringify(doc));
