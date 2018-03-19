1282b7733b629b799c270c775b51940ae5976715
Jonathan Ohayon <me@jonathano.com>
2018-03-20 00:06:45 +0200

    Fixed the front end CSV export function

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -175,7 +175,7 @@ app.controller('appCtrl', function ($rootScope, $scope, $http, $cookies, $locati
       navigator.msSaveBlob(blob, filename)
     } else {
       const link = document.createElement('a')
-      if (link.download) { // feature detection
+      if (link.download !== undefined) { // feature detection
         // Browsers that support HTML5 download attribute
         const url = URL.createObjectURL(blob)
         link.setAttribute('href', url)
