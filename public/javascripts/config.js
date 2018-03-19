8b120d81112a7ad190e6e619fe6bdf7473f3a734
Jonathan Ohayon <me@jonathano.com>
2018-03-19 20:49:24 +0200

    Standard.js-ified public/javascripts/config.js

--- public/javascripts/config.js
+++ public/javascripts/config.js
@@ -1,71 +1,72 @@
-app.config(function ($mdThemingProvider, $stateProvider, $urlRouterProvider) {
-    $mdThemingProvider.theme('default')
-        .primaryPalette('cyan')
-        .accentPalette('teal');
+/* global app */
+app.config(($mdThemingProvider, $stateProvider, $urlRouterProvider) => {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('cyan')
+    .accentPalette('teal')
 
-    $stateProvider.state({
-        name: 'team_picker',
-        url: '/',
-        templateUrl: '/views/team_picker.html',
-        controller: function($scope, $stateParams) {
-            $scope.clear_all();
-            $scope.pull_matches_from_tba();
-        }
-    });
+  $stateProvider.state({
+    name: 'team_picker',
+    url: '/',
+    templateUrl: '/views/team_picker.html',
+    controller: ($scope, $stateParams) => {
+      $scope.clear_all()
+      $scope.pull_matches_from_tba()
+    }
+  })
 
-    $stateProvider.state({
-        name: 'autonomous',
-        url: '/autonomous/',
-        templateUrl: '/views/autonomous.html'
-    });
+  $stateProvider.state({
+    name: 'autonomous',
+    url: '/autonomous/',
+    templateUrl: '/views/autonomous.html'
+  })
 
-    $stateProvider.state({
-        name: 'teleop',
-        url: '/teleop/',
-        templateUrl: '/views/teleop.html'
-    });
+  $stateProvider.state({
+    name: 'teleop',
+    url: '/teleop/',
+    templateUrl: '/views/teleop.html'
+  })
 
-    $stateProvider.state({
-        name: 'final_page',
-        url: '/final_page/',
-        templateUrl: '/views/final_page.html'
-    });
+  $stateProvider.state({
+    name: 'final_page',
+    url: '/final_page/',
+    templateUrl: '/views/final_page.html'
+  })
 
-    $stateProvider.state({
-        name: 'admin',
-        url: '/admin',
-        templateUrl: '/views/admin.html',
-        controller: function($scope, $stateParams) {
-            $scope.pull_teams_from_db();
-        }
-    });
-    
-    $stateProvider.state({
-        name: 'report',
-        url: '/report/:id',
-        templateUrl: '/views/report.html',
-        controller: function ($scope, $stateParams) {
-            $scope.get_cycle($stateParams.id);
-        }
-    });
-    
-    $stateProvider.state({
-        name: 'table',
-        url: '/table',
-        templateUrl: '/views/table.html',
-        controller: function ($scope, $stateParams) {
-            $scope.pull_matches_from_tba();
-            $scope.pull_match_team();
-        }
-    });
-    
-    $stateProvider.state({
-        name: 'export',
-        url: '/export',
-        controller: function ($scope, $stateParams) {
-            $scope.export_csv();
-        }
-    });
+  $stateProvider.state({
+    name: 'admin',
+    url: '/admin',
+    templateUrl: '/views/admin.html',
+    controller: ($scope, $stateParams) => {
+      $scope.pull_teams_from_db()
+    }
+  })
 
-    $urlRouterProvider.otherwise('/');
-});
+  $stateProvider.state({
+    name: 'report',
+    url: '/report/:id',
+    templateUrl: '/views/report.html',
+    controller: ($scope, $stateParams) => {
+      $scope.get_cycle($stateParams.id)
+    }
+  })
+
+  $stateProvider.state({
+    name: 'table',
+    url: '/table',
+    templateUrl: '/views/table.html',
+    controller: ($scope, $stateParams) => {
+      $scope.pull_matches_from_tba()
+      $scope.pull_match_team()
+    }
+  })
+
+  $stateProvider.state({
+    name: 'export',
+    url: '/export',
+    controller: ($scope, $stateParams) => {
+      $scope.export_csv()
+    }
+  })
+
+  $urlRouterProvider.otherwise('/')
+})
