3c11445f1a25bc581cfbf11ded64681fdcc0bb3a
Barak Shelef <shelef@guardicore.com>
2018-03-21 15:32:46 +0200

    Revert "Added myclick directive and updated auton page to use it for test"
    This reverts commit 05bc03b0027a2ddf56b764310463b2742c2bd960.

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -266,13 +266,3 @@ app.directive('capitalize', function () {
     }
   }
 })
-
-app.directive('myclick', function() {
-    return function(scope, element, attrs) {
-        element.bind('touchstart click', function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-            scope.$apply(attrs['myclick']);
-        });
-    };
-});
